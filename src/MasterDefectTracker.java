814bbd867e9979eaa6553e3a9e158607e88f02fe
Ashley Risius <arisius1@dmacc.edu>
2016-01-29 15:30:38 -0600

    uploading files

---
+++ src/MasterDefectTracker.java
@@ -0,0 +1,18 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class MasterDefectTracker {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JFrame master = new JFrame();
+		JPanel mainPanel = new MainPanel();
+		master.add(mainPanel);
+		
+		master.setSize(400, 250);
+		master.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		master.setVisible(true);
+
+	}
+
+}
